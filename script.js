6f375e7a3104a630bd8784e93d28928d72b2c4b4
Innovationbham_2 <Innovationbham_2@UABiLab-2s-iMac.local>
2017-06-16 12:46:46 -0500

    rough draft

---
+++ script.js
@@ -0,0 +1,13 @@
+var friends=['Goku','Vegeta','Gohan','Krillen','Bulma']
+
+for(var j=0;j<friends.length;j++){
+        for(var i=99;i>0;i--){
+            if(i>1){
+            console.log(i + ' lines of code in the file, ' + i + ' lines of code; ' + friends[j] + ' strikes one out, clears it all out, ');
+        }else{
+            console.log(' 1 line of code in the file, 1 line of code; ' + friends[j] + ' strikes one out, clears it all out, no more lines of code in the file');
+        }
+    }
+}  
+
+
