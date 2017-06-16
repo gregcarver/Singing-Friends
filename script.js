9716b022d1f7b195e4bd0e85af8a360079924b16
Innovationbham_2 <Innovationbham_2@UABiLab-2s-iMac.local>
2017-06-16 15:05:33 -0500

    final draft 2

--- script.js
+++ script.js
@@ -2,12 +2,12 @@ var friends=['Goku','Vegeta','Gohan','Krillen','Bulma']
 
 for(var j=0;j<friends.length;j++){
         for(var i=99;i>0;i--){
-            if(i>1){
-            console.log(i + ' lines of code in the file, ' + i + ' lines of code; ' + friends[j] + ' strikes one out, clears it all out, ' + (i-1) + ' lines of code in the file.');
-        }else{
-            console.log(' 1 line of code in the file, 1 line of code; ' + friends[j] + ' strikes one out, clears it all out, no more lines of code in the file.');
+            if(i>2){
+            console.log(i + ' lines of code in the file, ' + i + ' lines of code; ' + friends[j] + ' strikes one out, clears it all out, ' + (i-1) + ' lines of code in the file.')
+            }else if(i==2){
+            console.log(i + ' lines of code in the file, ' + i + ' lines of code; ' + friends[j] + ' strikes one out, clears it all out, ' + (i-1) + ' line of code in the file.')
+            }else if(i=1){
+            console.log(i + ' line of code in the file, ' + i + ' line of code; ' + friends[j] + ' strikes one out, clears it all out, no more lines of code in the file.')
         }
-    }
-}  
-
-
+    };
+};
